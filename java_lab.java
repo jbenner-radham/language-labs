46ca4bfbeefb8af435a4dfa73e5224225029d0c5
James Benner <james.benner@gmail.com>
2016-03-01 21:09:37 -0500

    Added initial Java lab file.

---
+++ java_lab.java
@@ -0,0 +1,9 @@
+public class java_lab {
+    public static void main(String[] args) {
+        String str = "Hello, world.";
+
+        System.out.println(str);
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
